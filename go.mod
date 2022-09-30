f3c9a08fce253ad09a3ed31785f309c8ce202611
vkotsiuba99 <92172262+vkotsiuba99@users.noreply.github.com>
2022-10-01 01:48:43 +0300

    refactor(rest): add env file

--- go.mod
+++ go.mod
@@ -23,6 +23,7 @@ require (
 	github.com/google/uuid v1.3.0 // indirect
 	github.com/gorilla/handlers v1.5.1 // indirect
 	github.com/gorilla/mux v1.8.0 // indirect
+	github.com/joho/godotenv v1.4.0 // indirect
 	github.com/klauspost/compress v1.11.13 // indirect
 	github.com/moby/locker v1.0.1 // indirect
 	github.com/moby/sys/mountinfo v0.5.0 // indirect
--- rest/main.go
+++ rest/main.go
@@ -5,11 +5,13 @@ import (
 	"encoding/json"
 	"github.com/gorilla/handlers"
 	"github.com/gorilla/mux"
+	"github.com/joho/godotenv"
 	"log"
 	"net/http"
 	"os"
 	"os/signal"
 	"sofi/sandbox"
+	"strings"
 	"syscall"
 	"time"
 )
@@ -71,12 +73,28 @@ func execute(w http.ResponseWriter, r *http.Request) {
 	}()
 }
 
+func loadOrigins(str string) []string {
+	result := strings.Split(str, ",")
+	for i := 0; i < len(result); i++ {
+		result[i] = strings.TrimSpace(result[i])
+	}
+
+	return result
+}
+
 func main() {
+	err := godotenv.Load("local.env")
+	if err != nil {
+		log.Fatalf("Error occurred while loading env file: %s", err)
+	}
+
+	origins := loadOrigins(os.Getenv("ORIGINS"))
+
 	router := mux.NewRouter().StrictSlash(true)
 	router.HandleFunc("/execute", execute).Methods(http.MethodPost)
 
 	headersOk := handlers.AllowedHeaders([]string{"X-Requested-With"})
-	originsOk := handlers.AllowedOrigins([]string{"http://localhost:3000"}) // TODO: Change to env variable
+	originsOk := handlers.AllowedOrigins(origins)
 	methodsOk := handlers.AllowedMethods([]string{"GET", "POST", "PUT"})
 
 	server := http.Server{
