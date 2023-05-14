2bbada052721237f3fcd1ffd60f3964cfd547aa0
vkotsiuba99 <92172262+vkotsiuba99@users.noreply.github.com>
2023-05-14 13:13:22 +0300

    docs(cache): add comments to cache implementation

--- internal/cache/cache.go
+++ internal/cache/cache.go
@@ -7,10 +7,14 @@ import (
 	"time"
 )
 
+// Cache is a generic cache implementation that can store and retrieve values of any type.
 type Cache[T any] struct {
+	// internal represents the internally redis cache.
 	internal *cache.Cache
 }
 
+// NewCache creates a new cache instance and returns a pointer to it. It also creates a
+// new local cache from redis which is a TinyLFU cache.
 func NewCache[T any]() *Cache[T] {
 	return &Cache[T]{internal: cache.New(&cache.Options{
 		Redis:      redisClient,
@@ -18,7 +22,10 @@ func NewCache[T any]() *Cache[T] {
 	})}
 }
 
+// Set stores a value in the cache for a given language and content key. The TTL is one
+// hour. It will also return an error, if an error occured while inserting the value.
 func (c *Cache[T]) Set(language, content string, value T) error {
+	// Create one key by encoding language and content.
 	key := encodeContent(language + content)
 	if err := c.internal.Set(&cache.Item{
 		Ctx:   context.Background(),
@@ -32,7 +39,10 @@ func (c *Cache[T]) Set(language, content string, value T) error {
 	return nil
 }
 
+// Get retrieves a value from the cache for a given language and content key. It will
+// return an error, if an error occured while getting the key or value.
 func (c Cache[T]) Get(language, content string) (T, error) {
+	// Encode content to get one key.
 	key := encodeContent(language + content)
 
 	var wanted T
@@ -43,15 +53,7 @@ func (c Cache[T]) Get(language, content string) (T, error) {
 	return wanted, nil
 }
 
+// encodeContent encodes a string into a base64 string.
 func encodeContent(content string) string {
 	return base64.StdEncoding.EncodeToString([]byte(content))
-}
-
-func decodeHash(hash string) (string, error) {
-	s, err := base64.StdEncoding.DecodeString(hash)
-	if err != nil {
-		return "", err
-	}
-
-	return string(s), nil
 }
